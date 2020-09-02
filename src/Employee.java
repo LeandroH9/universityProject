4b24019c17ff8a3931cc806640e589b30f58637e
Leandro Henrique <leandro.henrique@unesp.br>
2020-09-02 17:06:51 -0300

    arrumando algumas funcionalidades

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+
+public abstract class Employee {
+    private String code;
+    private String name;
+    private double salary;
+    private String level;
+    
+    public abstract float calculateWage();
+}
